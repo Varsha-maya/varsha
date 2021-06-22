96b84954adfde970bfa1cea377327005a004389c
Varsha-maya <86247429+Varsha-maya@users.noreply.github.com>
2021-06-22 22:48:34 +0530

    Update StudentArray.js

--- Nodejs/SRM--Batch2/StudentArray.js
+++ Nodejs/SRM--Batch2/StudentArray.js
@@ -5,6 +5,7 @@ Student[1] = { id: "2", name: "thaaru",  add:"6,nehru nagar", city: "Kovai", mar
 Student[2] = { id: "3", name: "maya", add:"8,nethaji nagar", city: "Madurai", marks: 39 };
 Student[3] = { id: "4", name: "ramya", add:"5,kalam nagar", city: "chennai", marks: 65 };
 Student[4] = { id: "5", name: "chen", add:"7,periyar nagar", city: "bengaluru", marks: 53 };
+
 Student.sort(function(a, b) {
     var nameA = a.name.toUpperCase(); 
     var nameB = b.name.toUpperCase(); 
@@ -16,10 +17,11 @@ Student.sort(function(a, b) {
     if (nameA > nameB) {
       return 1;
     }
-    
+    students.forEach((e) => {
+    console.log(`NAME: ${e.name} Stuednt id: ${e.id} Mark: ${e.marks} City: ${e.city}`)
   });
   console.log("|||||||||||||||||||||-----name sort-----|||||||||||||||||||||");
-    console.log(Student);
+    //console.log(Student);
     Student.sort(function (a,b){
         var CityA = a.city.toUpperCase(); 
     var CityB = b.city.toUpperCase();
