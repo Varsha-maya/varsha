5643478a8d683b82ba68af4f980fc06c5e1bc718
Varsha-maya <86247429+Varsha-maya@users.noreply.github.com>
2021-06-22 22:35:18 +0530

    Create StudentArray.js

---
+++ Nodejs/SRM--Batch2/StudentArray.js
@@ -0,0 +1,43 @@
+let Student = [];
+
+Student[0] = { id: "1", name: "varsha", add:"4,gandhi nagar", city: "Hosur", marks: 45 };
+Student[1] = { id: "2", name: "thaaru",  add:"6,nehru nagar", city: "Kovai", marks: 88 };
+Student[2] = { id: "3", name: "maya", add:"8,nethaji nagar", city: "Madurai", marks: 39 };
+Student[3] = { id: "4", name: "ramya", add:"5,kalam nagar", city: "chennai", marks: 65 };
+Student[4] = { id: "5", name: "chen", add:"7,periyar nagar", city: "bengaluru", marks: 53 };
+Student.sort(function(a, b) {
+    var nameA = a.name.toUpperCase(); 
+    var nameB = b.name.toUpperCase(); 
+   // console.log(nameA);
+    //console.log(nameB);
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    
+  });
+  console.log("|||||||||||||||||||||-----name sort-----|||||||||||||||||||||");
+    console.log(Student);
+    Student.sort(function (a,b){
+        var CityA = a.city.toUpperCase(); 
+    var CityB = b.city.toUpperCase();
+    if (CityA < CityB) {
+        return -1;
+      }
+      if (CityA > CityB) {
+        return 1;
+      }
+    
+    });
+    console.log("|||||||||||||||||||||-----city sort-----|||||||||||||||||||||");
+    console.log(Student);
+
+    let addmarks = Student.map(addmarks => {
+        let mar = addmarks.marks + 20;
+        console.log(`NAME: ${addmarks.name} Stuednt id: ${addmarks.id} Mark: ${mar} City: ${addmarks.city}`);
+    });
+    console.log("|||||||||||||||||||||-----add 20-----|||||||||||||||||||||");
+        console.log(Student);
+    
