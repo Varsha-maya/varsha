07250eaec694cd06d6a95203ef9cc8b16a8a8c91
Varsha-maya <86247429+Varsha-maya@users.noreply.github.com>
2021-06-22 15:01:01 +0530

    Update Inverse_Matrix.java

--- Java/SRM--Batch2/Inverse_Matrix.java
+++ Java/SRM--Batch2/Inverse_Matrix.java
@@ -17,8 +17,8 @@ public class Inverse_matrix {
 		mat[1][0] = - mat[1][0];
 		
 		System.out.println("\nInverse of matrix is:");
-		for(i = 0; i < 2; ++i) {
-			for(j = 0; j < 2; ++j)
+		for(int i = 0; i < 2; ++i) {
+			for(int j = 0; j < 2; ++j)
 				System.out.print((mat[i][j]/det) + " ");
 			
 			System.out.print("\n");
