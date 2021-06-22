ddb065c3bbc47a6af2fbad6888d97cc93981bae9
Varsha-maya <86247429+Varsha-maya@users.noreply.github.com>
2021-06-22 23:00:00 +0530

    Create Equaldiff.js

---
+++ Nodejs/SRM--Batch2/Equaldiff.js
@@ -0,0 +1,16 @@
+console.log("== and ===");
+var value4 = "188";
+var value5 = "1808";
+
+if (value4 == value5) {
+    console.log("TRUE");
+}
+else {
+    console.log("FALSE");
+}
+if (value4 === value5) {
+    console.log("Same data types");
+}
+else {
+    console.log("Different type");
+}
