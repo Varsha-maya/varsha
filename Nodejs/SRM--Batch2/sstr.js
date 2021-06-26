441d26828c4d0eb9fa17fb825056f7526e69cc8e
Varsha-maya <86247429+Varsha-maya@users.noreply.github.com>
2021-06-26 14:02:55 +0530

    Create sstr.js

---
+++ Nodejs/SRM--Batch2/sstr.js
@@ -0,0 +1,12 @@
+class stringdis{
+
+    static demo(){
+        let name="varsha";
+        let name1="thaaru";
+        //name1.split();
+        console.log(name.includes(name1));
+    
+    }
+    
+    }
+    stringdis.demo();
